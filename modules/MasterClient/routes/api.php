6d1ac6db2d0b27276a88310dc6a7b60574009986
Omar Ramirez <onmigregor@gmail.com>
2026-06-04 23:26:37 -0400

    feat: add api routes for MasterClient module with internal_key middleware

--- modules/MasterClient/routes/api.php
+++ modules/MasterClient/routes/api.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 use Modules\MasterClient\Http\Controllers\MasterClientController;
 
 Route::middleware(['internal_key'])->group(function () {
-    Route::get('/master-clients', [MasterClientController::class, 'index']);
-    Route::get('/master-clients/filters', [MasterClientController::class, 'getFilters']);
+    Route::get('/', [MasterClientController::class, 'index']);
+    Route::get('/filters', [MasterClientController::class, 'getFilters']);
     Route::post('/sync', [MasterClientController::class, 'sync']);
     Route::post('/sync-polar', [MasterClientController::class, 'syncPolar']);
 });
